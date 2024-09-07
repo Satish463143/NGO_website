09c6fc181e193c4e06e28bcc6e1b7ccabb313827
Satish <mahatosatish463@gmail.com>
2024-09-08 00:51:41 +0545

    worksection added

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../../component/Banner/Banner'
+import Work from '../../component/Work/Work'
 
 const Home = () => {
   return (
     <div>
         <Banner/>
+        <Work/>
     </div>
   )
 }
