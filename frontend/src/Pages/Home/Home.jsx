9d76260f9e8783d010518ecd5da6c382e558d900
Satish <mahatosatish463@gmail.com>
2024-09-08 14:40:29 +0545

    shortBAnner added

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../../component/Banner/Banner'
 import Work from '../../component/Work/Work'
+import ShortBanner from '../../component/ShortBanner/ShortBanner'
 
 const Home = () => {
   return (
     <div>
         <Banner/>
         <Work/>
+        <ShortBanner/>
     </div>
   )
 }
