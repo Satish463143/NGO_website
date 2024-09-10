4dbb29794a862431da74e9433e5f8bdaa7b36cd9
Prayusha Acharya <prayusha000@gmail.com>
2024-09-10 11:37:56 +0545

    correct path for all  logo.png

--- frontend/src/component/Footer/Footer.jsx
+++ frontend/src/component/Footer/Footer.jsx
@@ -8,31 +8,31 @@ const Footer = () => {
       <div className='container'>
         <div className='footer_grid'>
           <div className='logo_box'>
-            <img src="../src/assets/images/logo.png" alt="" />
+            <img src="/logo.png" alt="" />
             <p>
               You can use a few enticing words and flaunt your capabilities that will attract future donors and encourage them to donate right away.
             </p>
           </div>
           <div className='links'>
-            <ul><h3>Quick Links</h3> 
-            <Link><li>Home</li></Link>
-            <Link><li>What We Do</li></Link>
-            <Link><li>Our Partners</li></Link>
-            <Link><li>Gallery</li></Link>
-            <Link><li>Donate Us</li></Link>
-            </ul>            
+            <ul><h3>Quick Links</h3>
+              <Link><li>Home</li></Link>
+              <Link><li>What We Do</li></Link>
+              <Link><li>Our Partners</li></Link>
+              <Link><li>Gallery</li></Link>
+              <Link><li>Donate Us</li></Link>
+            </ul>
           </div>
           <div className='links'>
-            <ul><h3>Contact Info</h3> 
+            <ul><h3>Contact Info</h3>
               <li>702 Browning Homes,Lane No. 2,</li>
               <li>New York</li>
               <li>123-456-7890</li>
               <li>info@example.com</li>
-            </ul>            
+            </ul>
           </div>
           <div className='links'>
-            <ul><h3>Keep in Touch</h3> 
-            </ul>  
+            <ul><h3>Keep in Touch</h3>
+            </ul>
             <p>Connect with entrepreneurs, build your network, make great business.</p>
             <div className='media_links'>
               <a href=""><i class="fa-brands fa-square-facebook"></i></a>
@@ -40,7 +40,7 @@ const Footer = () => {
               <a href=""><i class="fa-solid fa-envelope"></i></a>
               <a href=""><i class="fa-brands fa-x-twitter"></i></a>
             </div>
-          </div>          
+          </div>
         </div>
         <div className='copyright'>
           <div>
--- frontend/src/component/Navbar/Navbar.jsx
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -1,68 +1,68 @@
 import React, { useState, useEffect } from 'react'
 import './Navbar.css'
-import {Link,useLocation} from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Navbar = () => {
     // const [isMenuActive, setMenuActive] = useState(false)
     const [menu, setMenu] = useState("Home")
     const location = useLocation()
 
-    const toogleNav = ()=>{
+    const toogleNav = () => {
         setMenuActive(!isMenuActive)
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         const path = location.pathname;
-        if(path === '/'){
+        if (path === '/') {
             setMenu("Home")
-        }else if(path === '/project'){
+        } else if (path === '/project') {
             setMenu("Our Initiative")
         }
-    },[location]);
-  return (
-    <div className='navbar'>
-        <div className='container'>
-            <div className='navbar_grid'>
-                <div className='logo_img'>
-                    <img src="../src/assets/images/logo.png" alt="Logo" />
-                </div>
-                <div className='navs'>
-                    <nav>
-                        <ul>
-                            <Link to="/">
-                                <li onClick={()=> {toogleNav(); setMenu("Home");}} className={`${menu === "Home"?'activeMenu':''}`} >Home</li>
-                            </Link>
-                            <Link>
-                                <li  onClick={()=> {toogleNav(); setMenu("");}} className={`${menu === ""?'activeMenu':''}`}>Who We Are</li>
-                            </Link>
-                            <Link>
-                                <li  onClick={()=> {toogleNav(); setMenu("");}} className={`${menu === ""?'activeMenu':''}`}>What We Do</li>
-                            </Link>
-                            <Link>
-                                <li  onClick={()=> {toogleNav(); setMenu("");}} className={`${menu === ""?'activeMenu':''}`}>Our Partners</li>
-                            </Link>
-                            <Link to='/project'>
-                                <li  onClick={()=> {toogleNav(); setMenu("");}} className={`${menu === "Our Initiative"?'activeMenu':''}`}>Our Initiative</li>
-                            </Link>                           
-                            <Link>
-                                <li  onClick={()=> {toogleNav(); setMenu("");}} className={`${menu === ""?'activeMenu':''}`}>Gallery</li>
-                            </Link>                           
-                            <Link>
-                                <li  onClick={()=> {toogleNav(); setMenu("");}} className={`${menu === ""?'activeMenu':''}`}>Contact Us</li>
-                            </Link>
-                                                      
-                        </ul>
-                    </nav>
-                </div>
-                <div className='donate_btn'>
-                <Link>
-                    <li  onClick={()=> {toogleNav(); setMenu("");}} className={`donate ${menu === ""?'activeMenu':''}`}>Donate</li>
-                </Link> 
+    }, [location]);
+    return (
+        <div className='navbar'>
+            <div className='container'>
+                <div className='navbar_grid'>
+                    <div className='logo_img'>
+                        <img src="/logo.png" alt="Logo" />
+                    </div>
+                    <div className='navs'>
+                        <nav>
+                            <ul>
+                                <Link to="/">
+                                    <li onClick={() => { toogleNav(); setMenu("Home"); }} className={`${menu === "Home" ? 'activeMenu' : ''}`} >Home</li>
+                                </Link>
+                                <Link>
+                                    <li onClick={() => { toogleNav(); setMenu(""); }} className={`${menu === "" ? 'activeMenu' : ''}`}>Who We Are</li>
+                                </Link>
+                                <Link>
+                                    <li onClick={() => { toogleNav(); setMenu(""); }} className={`${menu === "" ? 'activeMenu' : ''}`}>What We Do</li>
+                                </Link>
+                                <Link>
+                                    <li onClick={() => { toogleNav(); setMenu(""); }} className={`${menu === "" ? 'activeMenu' : ''}`}>Our Partners</li>
+                                </Link>
+                                <Link to='/project'>
+                                    <li onClick={() => { toogleNav(); setMenu(""); }} className={`${menu === "Our Initiative" ? 'activeMenu' : ''}`}>Our Initiative</li>
+                                </Link>
+                                <Link>
+                                    <li onClick={() => { toogleNav(); setMenu(""); }} className={`${menu === "" ? 'activeMenu' : ''}`}>Gallery</li>
+                                </Link>
+                                <Link>
+                                    <li onClick={() => { toogleNav(); setMenu(""); }} className={`${menu === "" ? 'activeMenu' : ''}`}>Contact Us</li>
+                                </Link>
+
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className='donate_btn'>
+                        <Link>
+                            <li onClick={() => { toogleNav(); setMenu(""); }} className={`donate ${menu === "" ? 'activeMenu' : ''}`}>Donate</li>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
