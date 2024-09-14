b891ac8cc104bf9067adcb827d2259be0602be6e
Satish <mahatosatish463@gmail.com>
2024-09-14 23:58:42 +0545

    hashrouter updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import StoreContextProvider from './context/StoreContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <StoreContextProvider>
         <App />
       </StoreContextProvider>    
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
