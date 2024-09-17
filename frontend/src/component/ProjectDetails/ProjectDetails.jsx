18e4e308c9125a57dbc9fd284abcd54d3dadc4e7
Prayusha Acharya <prayusha000@gmail.com>
2024-09-17 11:24:30 +0545

    Data fetch in project detail page

--- frontend/src/component/ProjectDetails/ProjectDetails.jsx
+++ frontend/src/component/ProjectDetails/ProjectDetails.jsx
@@ -3,50 +3,54 @@ import { StoreContext } from '../../context/StoreContext'
 import { useParams, Link } from 'react-router-dom'
 import './ProjectDetails.css';
 import { BASE_URL } from '../../../utils/api';
+import parse from 'html-react-parser';
+import { getAllProjects } from "../../api/ProjectApi";
 
 const ProjectDetails = () => {
     const { id } = useParams();
 
-    const [projectList, setProjectList] = useState([]);
+    const { data: projectList, error, loading } = getAllProjects();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`${BASE_URL}/project/get-all-projects`);
-                const result = await response.json();
-
-                setProjectList(result.data);
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        fetchData();
-    }, [projectList]);
-
-    const [project, setProject] = useState(null)
-    const [Loading, setLoading] = useState(true)
+    const [project, setProject] = useState(null);
 
     useEffect(() => {
         const foundProject = projectList.find(item => item._id === id);
         console.log(foundProject)
         setProject(foundProject);
-
-        setLoading(false);
     }, [id, projectList]);
 
-    if (Loading) return <div>Loading ...</div>
+    if (loading) return <div>Loading ...</div>
     if (!project) return <div>Project Not Found</div>
 
     return (
         <div>
             <h2>{project.title}</h2>
-            {/* {project.images.map((item, index) => ( */}
-            <div key={project.index}>
-                <img src={project.image1} alt="" />
-                <p >{project.description1}</p>
+            <div key={project._id}>
+                {project.description}
+
+                <ul style={{ listStyle: 'none', flex: 'col', textAlign: 'justify', justifyContent: 'space-between' }}>
+
+                    {project.description1 ? <li>{parse(project.description1)}</li> : ""}
+                    {project.image1 ? <li><img src={project.image1} /></li> : ""}
+
+                    {project.description2 ? <li>{parse(project.description2)} </li> : ""}
+                    {project.image2 ? <li><img src={project.image2} /></li> : ""}
+
+                    {project.description3 ? <li>{parse(project.description3)} </li> : ""}
+                    {project.image3 ? <li><img src={project.image3} /></li> : ""}
+
+                    {project.description4 ? <li>{parse(project.description4)} </li> : ""}
+                    {project.image4 ? <li><img src={project.image4} /></li> : ""}
+
+                    {project.description5 ? <li>{parse(project.description5)} </li> : ""}
+                    {project.image5 ? <li><img src={project.image5} /></li> : ""}
+
+                    {project.description6 ? <li>{parse(project.description6)} </li> : ""}
+                    {project.image6 ? <li><img src={project.image6} /></li> : ""}
+
+                </ul>
+
             </div>
-            {/* )) */
-            }
         </div>
     )
 }
