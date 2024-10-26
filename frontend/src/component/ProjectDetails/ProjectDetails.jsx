a8eb9b35df41f4bad267195e67997ee51174bf80
Prayusha Acharya <prayusha000@gmail.com>
2024-10-27 00:52:31 +0545

    add docs to the initiavies

--- frontend/src/component/ProjectDetails/ProjectDetails.jsx
+++ frontend/src/component/ProjectDetails/ProjectDetails.jsx
@@ -24,29 +24,15 @@ const ProjectDetails = () => {
     return (
         <div>
             <BannersItem title={project.title} image={nepalvillage} />
-            <div className="container">
-                <div className="iframe-container">
-                    <iframe
-                        src='https://docs.google.com/document/d/1MxpAjc29kMHyxONpDEE21Na_6ncANNYPerSOBVX09jk/pub?embedded=true'
-                        title="Project Document"
-                    />
-                </div>
+            <div className="detail_container">
                 <div key={project._id}>
                     {project.description}
-                    <ul style={{ listStyle: 'none', flexDirection: 'column', textAlign: 'justify', justifyContent: 'space-between' }}>
-                        {project.description1 ? <li>{parse(project.description1)}</li> : ""}
-                        {project.image1 ? <li><img src={project.image1} alt="Project Image 1" /></li> : ""}
-                        {project.description2 ? <li>{parse(project.description2)}</li> : ""}
-                        {project.image2 ? <li><img src={project.image2} alt="Project Image 2" /></li> : ""}
-                        {project.description3 ? <li>{parse(project.description3)}</li> : ""}
-                        {project.image3 ? <li><img src={project.image3} alt="Project Image 3" /></li> : ""}
-                        {project.description4 ? <li>{parse(project.description4)}</li> : ""}
-                        {project.image4 ? <li><img src={project.image4} alt="Project Image 4" /></li> : ""}
-                        {project.description5 ? <li>{parse(project.description5)}</li> : ""}
-                        {project.image5 ? <li><img src={project.image5} alt="Project Image 5" /></li> : ""}
-                        {project.description6 ? <li>{parse(project.description6)}</li> : ""}
-                        {project.image6 ? <li><img src={project.image6} alt="Project Image 6" /></li> : ""}
-                    </ul>
+                </div>
+                <div className="iframe-container">
+                    <iframe
+                        src={`${project.reportURL}?embedded=true`}
+                        title="Project Document"
+                    />
                 </div>
             </div>
         </div>
